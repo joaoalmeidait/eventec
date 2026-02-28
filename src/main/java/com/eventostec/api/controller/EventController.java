11f0cd4a678931b21a08639f4a8ecfdd1b9984ef
joaoalmeida <joaoalmeidapedrosa@gmail.com>
2026-02-27 21:48:54 -0300

    get upcoming events

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,14 +2,14 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/event")
@@ -44,4 +44,12 @@ public class EventController {
         Event newEvent = this.eventService.cerateEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
     }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page,size);
+        return ResponseEntity.ok(allEvents);
+    }
+
+
 }
