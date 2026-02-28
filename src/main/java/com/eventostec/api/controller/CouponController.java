28797f3c57bb39b6adf56ea45d487041e73bceb5
joaoalmeida <joaoalmeidapedrosa@gmail.com>
2026-02-27 21:48:44 -0300

    fix coupons

--- src/main/java/com/eventostec/api/controller/CouponController.java
+++ src/main/java/com/eventostec/api/controller/CouponController.java
@@ -5,10 +5,7 @@ import com.eventostec.api.domain.coupon.CouponRequestDTO;
 import com.eventostec.api.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,6 +16,7 @@ public class CouponController {
     @Autowired
     private CouponService couponService;
 
+    @PostMapping
     public ResponseEntity<Coupon> addCouponsToEvent(@PathVariable UUID eventID, @RequestBody CouponRequestDTO data){
         Coupon coupons = couponService.addCouponToEvent(eventID, data);
         return ResponseEntity.ok(coupons);
--- src/main/java/com/eventostec/api/service/CouponService.java
+++ src/main/java/com/eventostec/api/service/CouponService.java
@@ -6,10 +6,11 @@ import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.repositories.CouponRepository;
 import com.eventostec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.UUID;
-
+@Service
 public class CouponService {
 
     @Autowired
